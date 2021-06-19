c1899e0ee62eda6bfae630da295a23607fac73bc
Chris Kateera <kckateera@gmail.com>
2021-06-20 00:52:04 +0200

    [EDITED] Add timing to code sections

--- ASD.py
+++ ASD.py
@@ -1,8 +1,9 @@
 import matplotlib.pyplot as plt
-from modem import modulate, demodulate, HPF
+from modem import modulate, demodulate, hpf, modulate_old, demodulate_old
 from scipy.io import wavfile as wav
 from scipy.fftpack import rfft, irfft, fft, ifft, fftfreq
 import numpy as np
+import time
 
 
 class ASD:
@@ -14,11 +15,19 @@ class ASD:
         data = data.T[0]
         rate1, data1 = wav.read('./audios/vader.wav')  # 1CH
 
-        modulated_d1 = modulate(data1, rate1)
+        temp_time = time.time()
+        modulated_d1 = modulate_old(data1, rate1)
+        t0 = time.time() - temp_time
+
         modulated_d1.resize(data.shape)
+
+        temp_time = time.time()
         final = data + modulated_d1
+        t1 = time.time() - temp_time
 
         wav.write(filename="./audios/asd1-swsteganograph.wav", rate=rate, data=final)
+        print(f"elapsed time for modulation: {t0} overlaying: {t1}")
+
 
     def decoder(self):
         rate, final = wav.read('./audios/asd1-swsteganograph.wav')  # 2CH L,R
