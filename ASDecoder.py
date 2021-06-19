2721ae1e00c006c85f0b6678be6b41aee7312b0e
Chris Kateera <kckateera@gmail.com>
2021-06-19 16:12:09 +0200

    [EDITED] Decoder filter method

--- ASDecoder.py
+++ ASDecoder.py
@@ -1,11 +1,21 @@
-def main():
-    import matplotlib.pyplot as plt
-    from scipy.io import wavfile as wav
-    from scipy.fftpack import fft
-    import numpy as np
-    #rate, data = wav.read('./forceisstrong.wav')
-    rate, data = wav.read('./swthemesong.wav')
-    fft_out = fft(data)
-    plt.plot(data, np.abs(fft_out))
+import matplotlib.pyplot as plt
+from scipy import signal
+from scipy.io import wavfile as wav
+import numpy as np
+
+def HPF(data, rate):
+    t = np.arange(data.size)
+    sig=data
+    plot1 = plt.figure(1)
+    plt.plot([i for i in range(data[2000:2100].size)], data[2000:2100])
+
+    #Design a digital high-pass filter at 15 Hz to remove the 10 Hz tone, and apply it to the signal. (It’s recommended to use second-order sections format when filtering, to avoid numerical error with transfer function (ba) format):
+
+    sos = signal.butter(10, 11020, 'hp', fs=rate, output='sos')
+    filtered = signal.sosfilt(sos, sig)
+
     plt.show()
-main()
+    return filtered.astype(data.dtype)
+
+#filtered = HPF(final,sample)
+#wav.write(filename="./filtered.wav", rate=sample, data=filtered)
