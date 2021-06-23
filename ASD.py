ec27a685275afd78a624a255dce1286afc8177f0
Chris Kateera <kckateera@gmail.com>
2021-06-23 20:27:20 +0200

    [EDITED] Add plot labels

--- ASD.py
+++ ASD.py
@@ -54,86 +54,58 @@ class ASD:
         wav.write(filename="./audios/asd3-demodulated.wav", rate=self.rate, data=self.demodulated)
 
     def visualizer(self):
-        #ENCODING
+        # ENCODING
         plot1 = plt.figure(1)
         plt.title("Carrier audio")
-        plt.plot(np.arange(self.data[:60000].size), self.data[:60000])
-        plot2 = plt.figure(2)
-        plt.title("Secret")
-        plt.plot(np.arange(self.data1.size), self.data1)
-        plot3 = plt.figure(3)
-        plt.title("Modulated Secret")
-        plt.plot(np.arange(self.modulated_d1[:60000].size), self.modulated_d1[:60000])  # plot only first 60K compare
-        plot4 = plt.figure(4)
-        plt.title("Stegonagraph")
-        plt.plot(np.arange(self.final[:60000].size), self.final[:60000])  # plot only first 60K compare
+        plt.plot(np.arange(self.data.size), self.data)
+        plt.ylabel('Amplitude')
+        plt.xlabel('Time')
 
-        #DECODING
+        plot2 = plt.figure(2)
+        plt.title("Secret Message Signal")
+        plt.plot(np.arange(self.data1.size), self.data1)
+        plt.ylabel('Amplitude')
+        plt.xlabel('Time')
+
+        plot3 = plt.figure(3)
+        plt.title("AM Secret Message signal")
+        plt.plot(np.arange(self.modulated_d1[:60000].size), self.modulated_d1[:60000])  # plot only first 60K compare
+        plt.ylabel('Amplitude')
+        plt.xlabel('Time')
+
+        plot4 = plt.figure(4)
+        plt.title("Steganography Audio (Secret+Carrier)")
+        plt.plot(np.arange(self.final[:60000].size), self.final[:60000])  # plot only first 60K compare
+        plt.ylabel('Amplitude')
+        plt.xlabel('Time')
+
+        # DECODING
         plot5 = plt.figure(5)
-        plt.title("Filtered")
+        plt.title("HPF Filtered Steganography Audio")
         plt.plot(np.arange(self.filtered.size), self.filtered)
+        plt.ylabel('Amplitude')
+        plt.xlabel('Time')
+
         plot6 = plt.figure(6)
-        plt.title("Demodulated")
+        plt.title("Demodulated Secret Audio")
         plt.plot(np.arange(self.demodulated.size), self.demodulated)
+        plt.ylabel('Amplitude')
+        plt.xlabel('Time')
 
         plot7 = plt.figure(7)
-        plt.title("FFT")
-        plt.plot( linspace(-self.rate/2, self.rate/2, 46102), np.abs(fft(self.carrier))*10)
-        plt.plot( linspace(-self.rate/2, self.rate/2, 5801472), np.abs(fft(self.modulated_d1))*10)
-        plt.plot( fftfreq(self.data1.shape[0]), np.abs(fft(self.data1)))
-        # plt.legend = True
-        # r0 = fft(data) #[:20000] parallelize?
-        # r1 = fft(data1)
-        # r1.resize(r0.shape)
-        # r2 = np.abs(r0+r1)
-        #plt.show()
-        plots = [plot1,plot2,plot3,plot4,plot5,plot6,plot7]
+        plt.title("FFT Analysis")
+        plt.plot(linspace(-self.rate / 2, self.rate / 2, 46102), np.abs(fft(self.carrier)) * 10, color="purple")
+        plt.plot(linspace(-self.rate / 2, self.rate / 2, 5801472), np.abs(fft(self.modulated_d1)) * 10, color="red")
+        plt.plot(fftfreq(self.data1.shape[0]), np.abs(fft(self.data1)))
+        plt.ylabel('Magnitude')
+        plt.xlabel('Frequency')
+        plots = [plot1, plot2, plot3, plot4, plot5, plot6, plot7]
 
         for i in range(7):
-            #plot = plt.gcf()
             plots[i].set_size_inches(16, 9)
-            plots[i].show()
-            plots[i].savefig(f'ASD-{i}.png', dpi=100)
-
-
-        # plt.subplot(3,1,1)
-        # plt.title('Audio Steganography Process') #Amplitude Modulation
-        # plt.plot( self.data ,'g')
-        # plt.ylabel('Amplitude')
-        # plt.xlabel('Carrier Audio')
-        #
-        # plt.subplot(3,1,2)
-        # plt.plot(self.data1, 'r')
-        # plt.ylabel('Amplitude')
-        # plt.xlabel('Secret Message Signal')
-        #
-        # plt.subplot(3,1,3)
-        # plt.plot(self.modulated_d1[:60000], color="purple")
-        # plt.ylabel('Amplitude')
-        # plt.xlabel('AM Secret Message signal')
-
-        # plt.subplot(7,1,4)
-        # plt.plot(self.final[:60000], color="purple")
-        # plt.ylabel('Amplitude')
-        # plt.xlabel('Steganography Audio (AM Secret+Carrier)')
-        #
-        # plt.subplot(7,1,5)
-        # plt.plot(self.filtered, color="blue")
-        # plt.ylabel('Amplitude')
-        # plt.xlabel('HPF Filtered Steganography Audio')
-        #
-        # plt.subplot(7,1,6)
-        # plt.plot(self.demodulated, color="yellow")
-        # plt.ylabel('Amplitude')
-        # plt.xlabel('Demodulated Secret Audio')
-
-        # plt.subplots_adjust(hspace=1)
-        # plt.rc('font', size=15)
-        #
-        # fig = plt.gcf()
-        # fig.set_size_inches(16, 9)
-        # #plt.show()
-        # fig.savefig('Stego.png', dpi=100)
+            #plots[i].show()
+            plt.rc('font', size=35)
+            plots[i].savefig(f'./images/ASD-{i + 1}.png', dpi=100)
 
 
 if __name__ == "__main__":
@@ -141,4 +113,3 @@ if __name__ == "__main__":
     asd.encoder()
     asd.decoder()
     asd.visualizer()
-
