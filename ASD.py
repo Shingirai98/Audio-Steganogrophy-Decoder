480e074bf5741912a6bc72492f01536c0e14dd1f
Chris Kateera <kckateera@gmail.com>
2021-06-23 04:59:09 +0200

    [EDITED] make code OOP compliant

--- ASD.py
+++ ASD.py
@@ -8,60 +8,41 @@ import time
 
 class ASD:
     def __init__(self):
+        self.rate = self.rate1 = None
+        self.data = self.data1 = None
+        self.modulated_d1 = self.final = None
+        self.filtered = self.demodulated = None
         pass
 
     def encoder(self):
-        rate, data = wav.read('./audios/swthemesong.wav')  # 2CH L,R
-        data = data.T[0]
-        rate1, data1 = wav.read('./audios/vader.wav')  # 1CH
+        self.rate, self.data = wav.read('./audios/swthemesong.wav')  # 2CH L,R
+        self.data = self.data.T[0]
+        self.rate1, self.data1 = wav.read('./audios/vader.wav')  # 1CH
 
         temp_time = time.time()
-        modulated_d1 = modulate_old(data1, rate1)
+        self.modulated_d1 = modulate_old(self.data1, self.rate1)
         t0 = time.time() - temp_time
 
-        modulated_d1.resize(data.shape)
+        self.modulated_d1.resize(self.data.shape)
 
         temp_time = time.time()
-        final = data + modulated_d1
+        self.final = self.data + self.modulated_d1
         t1 = time.time() - temp_time
 
-        plot1 = plt.figure(1)
-        plt.title("Carrier audio")
-        plt.plot(np.arange(data.size), data)
-        plot2 = plt.figure(2)
-        plt.title("Secret")
-        plt.plot(np.arange(data1.size), data1)
-        plot3 = plt.figure(3)
-        plt.title("Modulated Secret")
-        plt.plot(np.arange(modulated_d1[:60000].size), modulated_d1[:60000]) #plot only first 60K compare
-        plot4 = plt.figure(4)
-        plt.title("Stegonagraph")
-        plt.plot(np.arange(final[:60000].size), final[:60000]) #plot only first 60K compare
-        plt.show()
-
-        wav.write(filename="./audios/asd0-modulated_secret.wav", rate=rate1, data=modulated_d1)
-        wav.write(filename="./audios/asd1-swsteganograph.wav", rate=rate, data=final)
+        wav.write(filename="./audios/asd0-modulated_secret.wav", rate=self.rate1, data=self.modulated_d1)
+        wav.write(filename="./audios/asd1-swsteganograph.wav", rate=self.rate, data=self.final)
         print(f"elapsed time for modulation: {t0} overlaying: {t1}")
-
 
     def decoder(self):
         rate, final = wav.read('./audios/asd1-swsteganograph.wav')  # 2CH L,R
 
         temp_time = time.time()
-        filtered = hpf(final, rate)
+        self.filtered = hpf(final, rate)
         t0 = time.time() - temp_time
 
         temp_time = time.time()
-        demodulated = demodulate_old(filtered, rate)
+        self.demodulated = demodulate_old(self.filtered, rate)
         t1 = time.time() - temp_time
-
-        plot1 = plt.figure(1)
-        plt.title("Filtered")
-        plt.plot(np.arange(filtered.size), filtered)
-        plot2 = plt.figure(2)
-        plt.title("Demodulated")
-        plt.plot(np.arange(demodulated.size), demodulated)
-        plt.show()
 
         print(f"Filtering time: {t0} demodulation: {t1}")
         wav.write(filename="./audios/asd2-filtered.wav", rate=rate, data=filtered)
